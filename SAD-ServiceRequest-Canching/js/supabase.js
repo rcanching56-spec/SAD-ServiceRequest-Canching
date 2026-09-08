753398bd9735226507503c7a5d3accea4ff0523a
rcanching56-spec <rcanching56@adssu.edu.ph>
2026-09-08 20:41:47 +0800

    Delete SAD-ServiceRequest-Canching directory

--- SAD-ServiceRequest-Canching/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.0/dist/module/index.js';
-
-const SUPABASE_URL = 'https://edududfqdabckdthztun.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_UeMvYZwobnBYxizHSmlMSA_d0tlmWUq';
-
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
